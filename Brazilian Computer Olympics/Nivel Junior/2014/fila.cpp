09521e6ba42b871495260c4d79beae6984379980
Thiago H. Cardoso <thiagohsc21@gmail.com>
2021-02-11 22:09:33 -0300

    +

---
+++ Brazilian Computer Olympics/Nivel Junior/2014/fila.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int removidos[1000005] = {0};
+
+int main(){
+
+    int n, fila[50005]; cin >> n;
+    for(int i = 0 ; i < n ; i++) cin >> fila[i];
+
+    int m; cin >> m;
+
+    for(int i = 0 ; i < m ; i++){
+        int x; cin >> x;
+        removidos[x] = 1;
+    }
+
+    for(int i = 0 ; i < n ; i++) if(removidos[fila[i]] == 0) cout << fila[i] << ' ';
+    return 0;
+}
