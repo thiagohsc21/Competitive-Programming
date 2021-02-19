9754a5c9ea7f953bddc0551705aa2bb167cdb27f
Thiago H. Cardoso <thiagohsc21@gmail.com>
2021-02-19 07:23:13 -0300

    +

---
+++ Brazilian Computer Olympics/Nivel 1/2012/corrida.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int c,n;
+    cin >> c >> n;
+
+    cout << c%n;
+    return 0;
+}
---
+++ Brazilian Computer Olympics/Nivel 1/2013/capital.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int x,y,z,w;
+    cin >> x >> y >> z >> w;
+
+    
+
+    return 0;
+}
