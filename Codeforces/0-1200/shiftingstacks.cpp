e62405923d266e1bf18ebe7c6888371e54a70f24
Thiago H. Cardoso <thiagohsc21@gmail.com>
2021-03-02 16:22:38 -0300

    +

--- Codeforces/0-1200/shiftingstacks.cpp
+++ Codeforces/0-1200/shiftingstacks.cpp
@@ -8,18 +8,15 @@ int main(){
     ll t;
     cin >> t;
     while(t--){
-        ll n,s=0;
+        ll n,s=0,sum=0,flag=0;
         cin >> n;
         for(ll i = 0 ; i < n ; i++){
             cin >> h[i];
-            s += h[i];
+            s += h[i]; sum+=i;
+            if(s < sum) flag = 1;
         }
-        ll pa = ((n-1)*n)/2;
-        if(s >= pa or n == 1 and ) cout << "YES" << endl;
+        if(flag == 0) cout << "YES" << endl;
         else cout << "NO" << endl;
-    }
-    // falta verificar se a soma ta bem
-    //distribuida ou concentrada
-     
+    }ma
     return 0;
 }
--- Codeforces/0-1200/template.cpp
+++ Codeforces/0-1200/template.cpp
@@ -1,9 +1,19 @@
-#include <bits/stdc++.h>
+no#include <bits/stdc++.h>
 using namespace std;
 
 #define ll long long int
+#define pb push_back
+#define 10000007 mod
 
-void main(){
-    
-    
+void resolution(){
+
+}
+
+int main(){
+    int t; 
+    cin >> t;
+    while(t--){
+        resolution();
+    }
+    return 0;
 }
